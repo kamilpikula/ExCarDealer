e6be5155bc116ec980ccfa7e9cc1fa4ff3c54277
kamilpikula <k.pikula97@gmail.com>
2020-12-21 16:44:56 +0100

    chore: update package for css

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule ExCarDealer.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      routes: "phx.routes"
     ]
   end
 end
