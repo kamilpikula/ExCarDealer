b377324ae49b2ec0fe4067bc5b90729eed345012
kamilpikula <k.pikula97@gmail.com>
2020-12-22 20:58:40 +0100

    fix: add dialyxir to test env

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule ExCarDealer.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
