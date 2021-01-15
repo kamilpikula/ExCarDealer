d0ae0f19ab98b045a098b89f3e7f9ef4551dae6c
kamilpikula <k.pikula97@gmail.com>
2021-01-15 22:05:50 +0100

    add role admin

--- lib/ex_car_dealer/users/user.ex
+++ lib/ex_car_dealer/users/user.ex
@@ -5,15 +5,24 @@ defmodule ExCarDealer.Users.User do
   schema "users" do
     field :first_name, :string
     field :last_name, :string
+    field :role, :string, null: false, default: "user"
     pow_user_fields()
 
     timestamps()
   end
 
+  @spec changeset(Ecto.Schema.t() | Ecto.Changeset.t(), map()) :: Ecto.Changeset.t()
   def changeset(user_or_changeset, attrs) do
     user_or_changeset
     |> pow_changeset(attrs)
     |> Ecto.Changeset.cast(attrs, [:first_name, :last_name])
     |> Ecto.Changeset.validate_length(:first_name, min: 3)
+  end
+
+  @spec changeset_role(Ecto.Schema.t() | Ecto.Changeset.t(), map()) :: Ecto.Changeset.t()
+  def changeset_role(user_or_changeset, attrs) do
+    user_or_changeset
+    |> Ecto.Changeset.cast(attrs, [:role])
+    |> Ecto.Changeset.validate_inclusion(:role, ~w(user admin))
   end
 end
